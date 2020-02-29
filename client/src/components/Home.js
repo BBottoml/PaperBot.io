7e96af7d8f023616f240ed64662bf7bbc73b4d52
lintopher0315 <lin978@purdue.edu>
2020-02-29 15:58:16 -0500

    home template

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,68 @@
 import React, { Component } from 'react';
+import { Container, Row, Col, Tabs, Tab, Table, Alert } from 'react-bootstrap';
 
-class Home extends React {
+import HomeHeader from './HomeHeader.js';
+import Graph from './Graph.js';
+
+class Home extends Component {
     render() {
         return (
-            <div></div>
+            <div>
+                <HomeHeader />
+                <Container fluid={true}>
+                    <Row>
+                        <Col>
+                            <Graph />
+                        </Col>
+
+                        <Col>
+                        </Col>
+                    </Row>
+
+                    <Row id="bottom-row">
+                        <Col>
+                            <Tabs defaultActiveKey="Tab1">
+                                <Tab eventKey="Tab1" title="Tab1">
+                                    <Table bordered>
+                                        <thead>
+                                            <tr>
+                                                <th>Actions</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th>
+                                                    <Alert variant={'info'}>
+                                                        12:34:43
+                                                        Bot performed action2
+                                                    </Alert>
+                                                </th>
+                                            </tr>
+                                            <tr>
+                                                <th>
+                                                    <Alert variant={'info'}>
+                                                        04:23:14
+                                                        Bot performed action1
+                                                    </Alert>
+                                                </th>
+                                            </tr>
+                                        </tbody>
+                                    </Table>
+                                </Tab>
+                                <Tab eventKey="Tab2" title="Tab2">
+                                    Random text 2
+                                </Tab>
+                                <Tab eventKey="Tab3" title="Tab3">
+                                    Random text 3
+                                </Tab>
+                            </Tabs>
+                        </Col>
+
+                        <Col>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         );
     }
 }
