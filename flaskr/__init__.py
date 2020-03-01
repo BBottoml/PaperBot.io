5510a50f64923cfebd5533069e0611b3bb450aee
18vijayb <18vijayb@students.harker.org>
2020-03-01 05:08:35 -0600

    Called algorithm processing upon addition of bot

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -47,10 +47,11 @@ def create_app(test_config=None):
     def create_bot():
         if (request.method=="POST"):
             form = request.values
-            name="Bot"+len(_bots.keys())
+            name="Bot"+str(len(_bots.keys()))
             algorithm = form['bot_algorithm']
             _bots[name]=algorithm.split()
             print(_bots)
+            process_algorithm()
             return "Successfully created/updated bot!"
         return "Error in creating bot!"
 
