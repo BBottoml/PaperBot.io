6236d9687b3f0df21ed4a9a4f37ffbf445dc48a7
lintopher0315 <lin978@purdue.edu>
2020-02-29 23:27:26 -0500

    add transition animation

--- client/src/components/Instructions.js
+++ client/src/components/Instructions.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Dropdown, Container, Row, Col, Button } from 'react-bootstrap';
+import { Animated } from 'react-animated-css';
 import Slider from 'react-slick';
 
 class Instructions extends Component {
@@ -20,78 +21,80 @@ class Instructions extends Component {
 
         return (
             <div>
-                <Container fluid={true}>
-                    <Row>
-                        <Col>
-                            <Slider id="instruction-slider" {...settings}>
-                                <div id="slider-div">
-                                    <p id="instruction-card">
-                                        Some quick example text to build on the card 
-                                        title and make up the bulk of the card's content.
-                                    </p>
-                                </div>
-                                <div id="slider-div">
-                                    <p id="instruction-card">
-                                        Some quick example text to build on the card 
-                                        title and make up the bulk of the card's content.
-                                    </p>
-                                </div>
-                                <div id="slider-div">
-                                    <p id="instruction-card">
-                                        Some quick example text to build on the card 
-                                        title and make up the bulk of the card's content.
-                                    </p>
-                                </div>
-                                <div id="slider-div">
-                                    <p id="instruction-card">
-                                        Some quick example text to build on the card 
-                                        title and make up the bulk of the card's content.
-                                    </p>
-                                </div>
-                                <div id="slider-div">
-                                    <p id="instruction-card">
-                                        Some quick example text to build on the card 
-                                        title and make up the bulk of the card's content.
-                                    </p>
-                                </div>
-                            </Slider>
-                        </Col>
+                <Animated animationIn="fadeInUp" animationOut="fadeOutUp" animationInDuration={1000} animationOutDuration={1000} isVisible={true}>
+                    <Container fluid={true}>
+                        <Row>
+                            <Col>
+                                <Slider id="instruction-slider" {...settings}>
+                                    <div id="slider-div">
+                                        <p id="instruction-card">
+                                            Some quick example text to build on the card 
+                                            title and make up the bulk of the card's content.
+                                        </p>
+                                    </div>
+                                    <div id="slider-div">
+                                        <p id="instruction-card">
+                                            Some quick example text to build on the card 
+                                            title and make up the bulk of the card's content.
+                                        </p>
+                                    </div>
+                                    <div id="slider-div">
+                                        <p id="instruction-card">
+                                            Some quick example text to build on the card 
+                                            title and make up the bulk of the card's content.
+                                        </p>
+                                    </div>
+                                    <div id="slider-div">
+                                        <p id="instruction-card">
+                                            Some quick example text to build on the card 
+                                            title and make up the bulk of the card's content.
+                                        </p>
+                                    </div>
+                                    <div id="slider-div">
+                                        <p id="instruction-card">
+                                            Some quick example text to build on the card 
+                                            title and make up the bulk of the card's content.
+                                        </p>
+                                    </div>
+                                </Slider>
+                            </Col>
 
-                        <Col>
-                            <div>
-                                <h1 id="title-text">Configure your Trading Bot</h1>
+                            <Col>
+                                <div>
+                                    <h1 id="title-text">Configure your Trading Bot</h1>
 
-                                <Container fluid={true}>
-                                    <Row>
-                                        <Col md={5}>
-                                            <Dropdown id="instruction-dropdown">
-                                                <Dropdown.Toggle variant="info">
-                                                    Select a Bot
-                                                </Dropdown.Toggle>
+                                    <Container fluid={true}>
+                                        <Row>
+                                            <Col md={5}>
+                                                <Dropdown id="instruction-dropdown">
+                                                    <Dropdown.Toggle variant="info">
+                                                        Select a Bot
+                                                    </Dropdown.Toggle>
 
-                                                <Dropdown.Menu>
-                                                    <Dropdown.Item>Bot 1</Dropdown.Item>
-                                                    <Dropdown.Item>Bot 2</Dropdown.Item>
-                                                    <Dropdown.Item>Bot 3</Dropdown.Item>
-                                                </Dropdown.Menu>
-                                            </Dropdown>
-                                        </Col>
+                                                    <Dropdown.Menu>
+                                                        <Dropdown.Item>Bot 1</Dropdown.Item>
+                                                        <Dropdown.Item>Bot 2</Dropdown.Item>
+                                                        <Dropdown.Item>Bot 3</Dropdown.Item>
+                                                    </Dropdown.Menu>
+                                                </Dropdown>
+                                            </Col>
 
-                                        <Col md={1}>
-                                            <Button id="trash-button" variant="danger">
-                                                <img src={require("../res/trash.png")}/>
-                                            </Button>
-                                        </Col>
+                                            <Col md={1}>
+                                                <Button id="trash-button" variant="danger">
+                                                    <img src={require("../res/trash.png")}/>
+                                                </Button>
+                                            </Col>
 
-                                        <Col md={6}>
-                                            <Button id="add-button" variant="success">+</Button>
-                                        </Col>
-                                    </Row>
-                                </Container>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
+                                            <Col md={6}>
+                                                <Button id="add-button" variant="success">+</Button>
+                                            </Col>
+                                        </Row>
+                                    </Container>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Animated>
             </div>
         );
     }
