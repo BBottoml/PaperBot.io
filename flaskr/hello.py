726c76537cb7e277b31da102c60179388fa7d9ba
Bradley Bottomlee <bradley.bttm@gmail.com>
2020-02-29 23:04:53 -0600

    Update weather API integration

--- flaskr/hello.py
+++ flaskr/hello.py
@@ -1,18 +1,40 @@
 import requests
 import sys
+import base64
 if __name__ == "__main__":
-    url = "https://community-open-weather-map.p.rapidapi.com/weather"
+    trend = sys.argv[2]
+    client_key = 'uFTmFW66AAMEUwx3rZlZDMSCf'
+    client_secret = 'LtlxIoQpBvHcqjpSMIA9Gs2E9wCJbr7xkx9EpSdBYoNedaZUgh'
 
-    querystring = {"callback":"test","id":"2172797","units":"%22metric%22 or %22imperial%22","mode":"xml%2C html","q":"Chicago"}
+    key_secret = '{}:{}'.format(client_key, client_secret).encode('ascii')
+    b64_encoded_key = base64.b64encode(key_secret)
+    b64_encoded_key = b64_encoded_key.decode('ascii')
 
-    headers = {
-        'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
-        'x-rapidapi-key': "3fa8a208f3mshac38fad42378d21p160849jsn2a5f80aa3204"
+    base_url = 'https://api.twitter.com/'
+    auth_url = '{}oauth2/token'.format(base_url)
+
+    auth_headers = {
+        'Authorization': 'Basic {}'.format(b64_encoded_key),
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    }
+    auth_data = {
+        'grant_type': 'client_credentials'
     }
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
+    auth_resp = requests.post(auth_url, headers=auth_headers, data=auth_data)
+    access_token = auth_resp.json()['access_token']
 
-    temp = float(response.text.split('"main":{"temp":')[1].split(",")[0])
-    temp = 1.8 * (temp - 273) + 32
-    print(temp)
-    #print(response.text)
+    search_headers = {
+        'Authorization': 'Bearer {}'.format(access_token)    
+    }
+    search_params = {
+        'id': 1,
+    }
+
+    search_url = '{}1.1/trends/place.json'.format(base_url)
+    search_resp = requests.get(search_url, headers=search_headers, params=search_params)
+    tweet_data = str(search_resp.json())
+
+    somebool = trend in tweet_data.lower()
+    print(somebool)
+    #return somebool
