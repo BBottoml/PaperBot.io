c299c8745cb8233d78f9d8ec77ca942d70b432d9
Anudeep Yakkala <ayakkala@purdue.edu>
2020-02-29 23:26:51 -0600

    fixed merge conflict

--- client/src/components/WebRouter.js
+++ client/src/components/WebRouter.js
@@ -3,13 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomeHeader from './HomeHeader.js';
 import Header from'./Header.js';
-import Home from './Home.js'
+import LandingPage from './LandingPage.js'
 
 function WebRouter() {
     return (
         <div>
             <Switch>
-                <Route exact path='/' component={() => <div><Header /><Home/></div>} />
+                <Route exact path='/' component={() => <div><Header /><LandingPage/></div>} />
                 <Route exact path='/home' component={() => <HomeHeader />} />
             </Switch>
         </div>
